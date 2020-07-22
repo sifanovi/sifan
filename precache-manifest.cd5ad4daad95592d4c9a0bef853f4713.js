42502a49c52f01d3881bddf17517ae12085e6826
sifan <sifan@sifans-MacBook-Air.local>
2020-07-22 21:49:09 +0600

    pushing

---
+++ precache-manifest.cd5ad4daad95592d4c9a0bef853f4713.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cafbf710ea8131a1c74973a1603b7271",
+    "url": "/index.html"
+  },
+  {
+    "revision": "223adc5eef9bd40becc9",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4e540950fa629326cbfb",
+    "url": "/static/css/main.5520a793.chunk.css"
+  },
+  {
+    "revision": "223adc5eef9bd40becc9",
+    "url": "/static/js/2.e5f40c05.chunk.js"
+  },
+  {
+    "revision": "0588ddb6bdc8949d9c8bc082d889696c",
+    "url": "/static/js/2.e5f40c05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e540950fa629326cbfb",
+    "url": "/static/js/main.6a842325.chunk.js"
+  },
+  {
+    "revision": "052274ddd0fd19653d57",
+    "url": "/static/js/runtime-main.e13232da.js"
+  },
+  {
+    "revision": "3f1852cfeff04acabaf26f99f836ed6c",
+    "url": "/static/media/background.3f1852cf.jpg"
+  }
+]);
